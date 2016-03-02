8220669d2c826d7120959352bc4707fa299810a5
David Guan <DavidGuanDev@gmail.com>
2016-03-02 21:28:11 +0800

    Set up develop environment.

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+import d3 from 'd3'
+
+import './home.scss'
+
+const titleArray = []
+for (let i = 1; i < 21; i++) {
+  titleArray.push(`day ${i}`)
+}
+
+const areaTags = d3.select('body').selectAll('div')
+  .data(titleArray)
+  .enter()
+  .append('div')
+  .attr('class', 'main-area')
+
+areaTags.append('p').text(d => d)
+areaTags.append('svg')
+
+console.log(1)
+require ('./day1.js')
