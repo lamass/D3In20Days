cd5a5a353d47d198e9c7c1d162dd0f563130d170
David Guan <DavidGuanDev@gmail.com>
2016-03-18 23:14:19 +0800

    Add webpack config for prod.

---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [ './scripts/app.js' ],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }, {
+        test: /\.css$/,
+        loader: 'style!css'
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+        dead_code: true,
+      }
+    }),
+  ]
+}
