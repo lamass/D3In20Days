305beb9acb4bc24fc245e370742c8f3856285771
David Guan <DavidGuanDev@gmail.com>
2016-04-16 19:42:10 +0800

    Add animation.

--- scripts/day19.js
+++ scripts/day19.js
@@ -1,19 +1,31 @@
 //Better demo of exit and transition
 import d3 from 'd3'
 
+import './day19.scss'
+
 
 const currentDay = 19
 
 const svgArea = document.querySelectorAll('svg')[currentDay - 1]
+svgArea.id = `day${currentDay}`
 
 function render(data) {
   const selection = d3.select(svgArea).selectAll('text')
     .data(data, d => d)
 
-  selection.exit().remove()
+  selection.exit()
+    .transition()
+    .duration(1000)
+    .attr({
+      transform: (d, i) => `translate(${50 + i * 20}, 400)`
+    })
+    .remove()
 
   selection
+    .transition()
+    .duration(1000)
     .attr({
+      fill: '#0086b3',
       transform: (d, i) => `translate(${50 + i * 20}, 200)`
     })
 
@@ -24,8 +36,13 @@ function render(data) {
     })
 }
 
-setInterval(() => {
-  const data = 'abcdefghijklmn'.split('')
+function refresh() {
+  const data = 'abcdefghijk'.split('')
   const randomArr = d3.shuffle(data).slice(Number.parseInt(Math.random() * data.length))
   render(randomArr)
+}
+refresh()
+
+setInterval(() => {
+  refresh()
 }, 2000)
