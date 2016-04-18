adfc7b8968a16a4b76c0489bc42d386c9d55f65e
David Guan <DavidGuanDev@gmail.com>
2016-04-18 18:53:27 +0800

    Add map of boston.

---
+++ scripts/day20.js
@@ -0,0 +1,27 @@
+import d3 from 'd3'
+
+import data from './day20_assets/boston.geojson'
+import './day20.scss'
+
+const currentDay = 20
+const svgWidth = 400
+const svgHeight = 400
+
+const svgArea = document.querySelectorAll('svg')[currentDay - 1]
+svgArea.id = `day${currentDay}`
+// const color = d3.scale.category20()
+
+const projection = d3.geo.conicEqualArea()
+  // .scale(200)
+  // .translate([svgWidth, -svgHeight])
+const geoPath = d3.geo.path().projection(projection)
+d3.select(svgArea).append('g')
+  .selectAll('path')
+  .data(data.features)
+  .enter()
+  .append('path')
+  .attr({
+    class: 'countries',
+    d: geoPath,
+    fill: 'red'
+  })
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url?limit=10000&mimetype=image/svg+xml'
       }, {
-        test: /\.json$/,
+        test: /\.(json)|(geojson)$/,
         loader: 'json'
       },
     ]
