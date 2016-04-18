c8354948a1110f5f62b022ddcd91f321f150ea6a
David Guan <DavidGuanDev@gmail.com>
2016-04-18 18:56:39 +0800

    Fix prod build config incorrect.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -35,7 +35,7 @@ module.exports = {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url?limit=10000&mimetype=image/svg+xml'
       }, {
-        test: /\.json$/,
+        test: /\.(json)|(geojson)$/,
         loader: 'json'
       },
     ]
